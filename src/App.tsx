c6cd1270df1cccbc398b95f88f4e408f492a550f
Saul <yosoberana1@gmail.com>
2026-01-31 19:36:48 -0600

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {Button} from './components/ui/button'
+import {Card} from './components/ui/card'
+
+function App() {
+ return <div className="grid grid-cols-3 grid-rows-3 p-12 bg-amber-300 ">
+  <div className='border-black border-2'>1</div>
+  <Card/>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+
+ </div>
+ 
+  
+
+}
+
+export default App
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return (
+    <div className=" flex flex-col border-2 border-black m-4 gap-2.5 text-center">
+    <p>Esta es una tarjeta</p>
+    </div>
+    )
+    
+    
+}
+
+export { Card };
