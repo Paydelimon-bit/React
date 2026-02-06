f1a8464ee003ba0f66c231c2fd745772bb7844d9
Saul <yosoberana1@gmail.com>
2026-02-06 10:11:20 -0600

    Sepa

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import {Card} from './components/ui/card'
 
 function App() {
  return <div className="grid grid-cols-3 grid-rows-3 p-12 bg-amber-300 ">
-  <div className='border-black border-2'>1</div>
   <Card/>
-  <div>3</div>
-  <div>4</div>
-  <div>5</div>
-  <div>6</div>
-  <div>7</div>
-  <div>8</div>
-  <div>9</div>
-
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
  </div>
  
   
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,8 +1,8 @@
 
 function Card(){
     return (
-    <div className=" flex flex-col border-2 border-black m-4 gap-2.5 text-center">
-    <p>Esta es una tarjeta</p>
+    <div className=" flex flex-col border-2 border-black m-3 gap-2.5 text-center">
+    <p className="p-10 font-bold">Esta es una tarjeta</p>
     </div>
     )
     
